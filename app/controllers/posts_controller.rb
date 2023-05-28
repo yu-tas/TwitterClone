8b9253813c43c2de6d376cdbef6623c33c201e99
shintaniyuta <cada3986@yahoo.co.jp>
2023-05-28 11:05:31 +0900

    入門23まで実装完了

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,15 +1,29 @@
 class PostsController < ApplicationController
   def index
+    @posts = Post.all
   end
 
   def new
-    @post = Post.new                                    #controllerでデータを作りviewからブラウザ表示
+    @post = Post.new                                         #controllerでデータを作りviewからブラウザ表示
   end 
 
   def create
-    Post.create(content: params[:post][:content])       #送られてきたデータをパラメーターで取得
-    redirect_to new_post_path
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to posts_path, notice: "tweetを作成しました！"
+    else
+      render :new  
+    end                            
   end
+
+  def show
+    @post = Post.find(params[:id])
+  end  
   
+private                                                       #予期せぬところからblog_paramsを呼び出せないようにするため
+
+  def post_params
+    params.require(:post).permit(:content)                    #送られてきたデータをパラメーターで取得
+  end
 end
 
